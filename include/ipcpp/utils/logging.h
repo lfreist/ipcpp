25270046320eb153578f411ba9ab3166ea52a909
Leon Freist <freist.leon@gmail.com>
2024-12-20 12:10:30 +0100

    logging fix: compile time set logging level was buggy

--- include/ipcpp/utils/logging.h
+++ include/ipcpp/utils/logging.h
@@ -7,105 +7,104 @@
 
 #pragma once
 
-#define LOG_LEVEL_OFF 0
-#define LOG_LEVEL_TRACE 1
-#define LOG_LEVEL_DEBUG 2
-#define LOG_LEVEL_INFO 3
-#define LOG_LEVEL_WARN 4
-#define LOG_LEVEL_ERROR 5
-#define LOG_LEVEL_CRITICAL 6
+#define LOG_LEVEL_TRACE 0
+#define LOG_LEVEL_DEBUG 1
+#define LOG_LEVEL_INFO 2
+#define LOG_LEVEL_WARN 3
+#define LOG_LEVEL_ERROR 4
+#define LOG_LEVEL_CRITICAL 5
+#define LOG_LEVEL_OFF 6
 
 #ifndef LOGGING_LEVEL
 #define LOGGING_LEVEL LOG_LEVEL_INFO
 #endif
-#if LOGGING_LEVEL > LOG_LEVEL_OFF
 #include <spdlog/spdlog.h>
-#else
-#pragma message("Logging is disabled");
+#if LOGGING_LEVEL == LOG_LEVEL_OFF
+#pragma message("Logging is disabled")
 #endif
 
 namespace ipcpp::logging {
 
 template <typename T>
 inline void trace(const T& msg) {
-#if LOGGING_LEVEL >= LOG_LEVEL_TRACE
+#if LOGGING_LEVEL <= LOG_LEVEL_TRACE
   spdlog::trace(msg);
 #endif
 }
 
 template <typename T>
 inline void debug(const T& msg) {
-#if LOGGING_LEVEL >= LOG_LEVEL_DEBUG
+#if LOGGING_LEVEL <= LOG_LEVEL_DEBUG
   spdlog::debug(msg);
 #endif
 }
 
 template <typename T>
 inline void info(const T& msg) {
-#if LOGGING_LEVEL >= LOG_LEVEL_INFO
+#if LOGGING_LEVEL <= LOG_LEVEL_INFO
   spdlog::info(msg);
 #endif
 }
 
 template <typename T>
 inline void warn(const T& msg) {
-#if LOGGING_LEVEL >= LOG_LEVEL_WARN
+#if LOGGING_LEVEL <= LOG_LEVEL_WARN
   spdlog::warn(msg);
 #endif
 }
 
 template <typename T>
 inline void error(const T& msg) {
-#if LOGGING_LEVEL >= LOG_LEVEL_ERROR
+#if LOGGING_LEVEL <= LOG_LEVEL_ERROR
   spdlog::error(msg);
 #endif
 }
 
 template <typename T>
 inline void critical(const T& msg) {
-#if LOGGING_LEVEL >= LOG_LEVEL_CRITICAL
+#if LOGGING_LEVEL <= LOG_LEVEL_CRITICAL
   spdlog::critical(msg);
 #endif
 }
 
 template <typename... Args>
 inline void trace(fmt::format_string<Args...> s, Args&&... args) {
-#if LOGGING_LEVEL >= LOG_LEVEL_TRACE
+#if LOGGING_LEVEL <= LOG_LEVEL_TRACE
   spdlog::trace(s, std::forward<Args>(args)...);
 #endif
 }
 
 template <typename... Args>
 inline void debug(fmt::format_string<Args...> s, Args&&... args) {
-#if LOGGING_LEVEL >= LOG_LEVEL_DEBUG
+#if LOGGING_LEVEL <= LOG_LEVEL_DEBUG
   spdlog::debug(s, std::forward<Args>(args)...);
 #endif
 }
 
 template <typename... Args>
 inline void info(fmt::format_string<Args...> s, Args&&... args) {
-#if LOGGING_LEVEL >= LOG_LEVEL_INFO
+#if LOGGING_LEVEL <= LOG_LEVEL_INFO
   spdlog::info(s, std::forward<Args>(args)...);
 #endif
 }
 
 template <typename... Args>
 inline void warn(fmt::format_string<Args...> s, Args&&... args) {
-#if LOGGING_LEVEL >= LOG_LEVEL_WARN
+#if LOGGING_LEVEL <= LOG_LEVEL_WARN
   spdlog::warn(s, std::forward<Args>(args)...);
 #endif
 }
 
 template <typename... Args>
 inline void error(fmt::format_string<Args...> s, Args&&... args) {
-#if LOGGING_LEVEL >= LOG_LEVEL_ERROR
+#if LOGGING_LEVEL <= LOG_LEVEL_ERROR
   spdlog::error(s, std::forward<Args>(args)...);
 #endif
 }
 
 template <typename... Args>
 inline void critical(fmt::format_string<Args...> s, Args&&... args) {
-#if LOGGING_LEVEL >= LOG_LEVEL_CRITICAL
+#if LOGGING_LEVEL <= LOG_LEVEL_CRITICAL
   spdlog::critical(s, std::forward<Args>(args)...);
 #endif
 }
